2e3fd9b2431524334d7f02fedb543e317107bc70
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-05-19 14:03:52 -0300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "tomasulo";
+        packages = with pkgs; [
+          gnumake
+          clang
+        ];
+      };
+    });
+}
